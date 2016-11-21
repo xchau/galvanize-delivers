e1826bdb61fbc4f0b565c415619dddcc15281074
Minh Chau <xiu.chau@gmail.com>
2016-11-20 19:13:26 -0800

    completed validation; bug = overlapping text

--- js/order.js
+++ js/order.js
@@ -1,7 +1,11 @@
-$('.button-collapse').sideNav(); // materialize init
-
 (function() {
   'use strict';
+
+  // MATERIALIZE
+  $('.button-collapse').sideNav();
+  $(document).ready(function() {
+    $('input#input_text, textarea#textarea1').characterCounter();
+  });
 
   // TABLE SELECTORS
   const $allButtonsDiv = $('#allButtonsDiv');
@@ -44,7 +48,7 @@ $('.button-collapse').sideNav(); // materialize init
       $tdRoyaleQuantity.addClass('center-align').text(quantity[0]);
       $tdRoyalePrice.addClass('right-align').text(price);
       $trRoyale.append($tdRoyale, $tdRoyaleQuantity, $tdRoyalePrice);
-      $tbody.append($trRoyale);
+      $tbody.prepend($trRoyale);
     }
     if (name === 'Arugula Pie') {
       quantity[1] += 1;
@@ -52,7 +56,7 @@ $('.button-collapse').sideNav(); // materialize init
       $tdArugulaQuantity.addClass('center-align').text(quantity[1]);
       $tdArugulaPrice.addClass('right-align').text(price);
       $trArugula.append($tdArugula, $tdArugulaQuantity, $tdArugulaPrice);
-      $tbody.append($trArugula);
+      $tbody.prepend($trArugula);
     }
     if (name === 'Smoked Swine') {
       quantity[2] += 1;
@@ -60,7 +64,7 @@ $('.button-collapse').sideNav(); // materialize init
       $tdRibsQuantity.addClass('center-align').text(quantity[2]);
       $tdRibsPrice.addClass('right-align').text(price);
       $trRibs.append($tdRibs, $tdRibsQuantity, $tdRibsPrice);
-      $tbody.append($trRibs);
+      $tbody.prepend($trRibs);
     }
     if (name === 'Ice Cream Biscuit') {
       quantity[3] += 1;
@@ -68,7 +72,7 @@ $('.button-collapse').sideNav(); // materialize init
       $tdBiscuitQuantity.addClass('center-align').text(quantity[3]);
       $tdBiscuitPrice.addClass('right-align').text(price);
       $trBiscuit.append($tdBiscuit, $tdBiscuitQuantity, $tdBiscuitPrice);
-      $tbody.append($trBiscuit);
+      $tbody.prepend($trBiscuit);
     }
   };
 
@@ -85,6 +89,7 @@ $('.button-collapse').sideNav(); // materialize init
   const genTax = function() {
     const subtotalRegex = $currentSubtotal.text().match(/(\$*)(\d+.\d*)/);
     const tax = (parseFloat(subtotalRegex[2]) * 0.096).toFixed(2);
+
     $currentTax.text(`$${tax}`);
   };
 
@@ -92,7 +97,10 @@ $('.button-collapse').sideNav(); // materialize init
   const genTotal = function() {
     const subtotalRegex = $currentSubtotal.text().match(/(\$*)(\d+.\d*)/);
     const taxRegex = $currentTax.text().match(/(\$*)(\d+.\d*)/);
-    const grandTotal = (parseFloat(subtotalRegex[2]) + parseFloat(taxRegex[2])).toFixed(2);
+    const sub = parseFloat(subtotalRegex[2]);
+    const tax = parseFloat(taxRegex[2]);
+    const grandTotal = (sub + tax).toFixed(2);
+
     $currentTotal.text(`$${grandTotal}`);
   };
 
@@ -107,5 +115,77 @@ $('.button-collapse').sideNav(); // materialize init
     genSubtotal($foodPrice);
     genTax();
     genTotal();
+  });
+
+  const warning = 'This field is required*';
+  const $nameField = $('#name');
+
+  // FOCUS IN & OUT in EMPTY NAME FIELD
+  $nameField.focusout(() => {
+    if (!$nameField.prop('value')) {
+      $nameField.prop('placeholder', warning);
+    }
+  });
+  $nameField.focusin(() => {
+    $nameField.prop('placeholder', '');
+  });
+
+  const $phoneField = $('#phoneNumber');
+
+  // FOCUS IN & OUT in EMPTY PHONE FIELD
+  $phoneField.focusout(() => {
+    if (!$phoneField.prop('value')) {
+      $phoneField.prop('placeholder', warning);
+    }
+  });
+  $phoneField.focusin(() => {
+    $phoneField.prop('placeholder', '');
+  });
+
+  const $addressField = $('#address');
+
+  // FOCUS IN & OUT in EMPTY PHONE FIELD
+  $addressField.focusout(() => {
+    if (!$addressField.prop('value')) {
+      $addressField.prop('placeholder', warning);
+    }
+  });
+  $addressField.focusin(() => {
+    $addressField.prop('placeholder', '');
+  });
+
+  // VALIDATE TEMPLATE ON SUBMISSION
+  const $form = $('#user-info-form');
+  const nameRegEx = /([a-zA-Z-]+)( )([a-zA-Z-]+)/g;
+  const phoneRegEx = /[0-9-().# ]/g;
+  const addressRegEx = /[a-zA-Z0-9-#. ]/g;
+  const nameWarning = 'Only alphabet characters allowed';
+  const phoneWarning = 'Enter a valid phone number';
+  const addressWarning = 'Enter a valid address';
+
+  $form.submit((event) => {
+    event.preventDefault();
+
+    if (!nameRegEx.test($nameField.val())) {
+      $nameField.prop('value', '');
+      $nameField.prop('placeholder', nameWarning);
+      $nameField.focusin(() => {
+        $nameField.prop('placeholder', '');
+      });
+    }
+    if (!phoneRegEx.test($phoneField.val())) {
+      $phoneField.prop('value', '');
+      $phoneField.prop('placeholder', phoneWarning);
+      $phoneField.focusin(() => {
+        $phoneField.prop('placeholder', '');
+      });
+    }
+    if (!addressRegEx.test($addressField.val())) {
+      $addressField.prop('value', '');
+      $addressField.prop('placeholder', addressWarning);
+      $addressField.focusin(() => {
+        $addressField.prop('placeholder', '');
+      });
+    }
   });
 })();
